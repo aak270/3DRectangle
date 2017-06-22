81fe243acc1286d863fd488970ad27db8746b709
Anggoro Gagah <arahardiyawan@zamanuniversity.edu.kh>
2017-06-22 21:58:17 +0700

    c file

---
+++ 3DRectangle.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct 3Dpoint{
+	double x;
+	double y;
+	double z;
+};
+
+struct 3DRectangle{
+	//properties
+};
+
+/********functions***********/
+
+int main(void) {
+	
+	return 0;
+}
