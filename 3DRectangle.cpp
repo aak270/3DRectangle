626dfd11ca29fffaa78760964c14ffbd802d4462
Anggoro Gagah <arahardiyawan@zamanuniversity.edu.kh>
2017-06-23 11:38:54 +0700

    add new

--- 3DRectangle.cpp
+++ 3DRectangle.cpp
@@ -2,17 +2,30 @@
 
 /********structs*************/
 
-struct 3Dpoint{
+struct point3D{
 	double x;
 	double y;
 	double z;
 };
 
-struct 3DRectangle{
-	//properties
+//with all angle 90 degree
+struct rectangle3D{
+	struct point3D a;
+	struct point3D b;
+	double height;
 };
 
 /********functions***********/
+
+//calculate the surface area for 3d rectangle
+double getSurfaceArea(struct rectangle3D){
+	
+}
+
+//calculate the volume for 3d rectangle
+double getVolume(struct rectangle3D){
+	
+}
 
 void members(){
 	printf("Anggoro Rahardiyawan\n");
